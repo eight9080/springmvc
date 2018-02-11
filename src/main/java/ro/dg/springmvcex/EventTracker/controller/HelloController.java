dbc9741f42463ef9c01ced8a13873c67341dc6f9
eight9080 <eight9080@gmail.com>
2018-02-11 23:53:48 +0200

    controllers spring mvc 4 - no web.xml

--- src/main/java/ro/dg/springmvcex/EventTracker/controller/HelloController.java
+++ src/main/java/ro/dg/springmvcex/EventTracker/controller/HelloController.java
@@ -1,4 +1,4 @@
-package ro.dg.springmvcex.EventTracker;
+package ro.dg.springmvcex.EventTracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+
     @RequestMapping("/hello")
     public String hello(Model model/*, @RequestParam(value="name", required=false, defaultValue="World") String name*/) {
 //        model.addAttribute("name", name);
         return "hello";
+    }
+
+    @RequestMapping(value = "/greeting")
+    public String sayHello(Model model){
+        model.addAttribute("greeting", "Hello World");
+        return "hello1";
     }
 }
---
+++ src/main/webapp/hello1.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Index</title>
+</head>
+<body>
+<h1> Hello - hello</h1>
+<h1>${greeting}</h1>
+</body>
+</html>
