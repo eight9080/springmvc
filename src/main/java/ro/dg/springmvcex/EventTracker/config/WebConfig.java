ce9db1ecea864a66ab4f9ddb031413ebc7dcd58c
eight9080 <eight9080@gmail.com>
2018-02-16 00:15:51 +0200

    i18n

--- src/main/java/ro/dg/springmvcex/EventTracker/config/WebConfig.java
+++ src/main/java/ro/dg/springmvcex/EventTracker/config/WebConfig.java
@@ -1,14 +1,42 @@
 package ro.dg.springmvcex.EventTracker.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        LocaleChangeInterceptor changeInterceptor = new LocaleChangeInterceptor();
+        changeInterceptor.setParamName("language");
+        registry.addInterceptor(changeInterceptor);
+    }
 
     @Bean
     public InternalResourceViewResolver getViewResolver(){
--- src/main/webapp/WEB-INF/jsp/attendee.jsp
+++ src/main/webapp/WEB-INF/jsp/attendee.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 
 <!DOCTYPE html>
 <html>
@@ -20,13 +21,17 @@
 </head>
 <body>
 <h1> Hello - events</h1>
+
+<a href="?language=en">English</a>
+<a href="?language=es">Espaniol</a>
+
 <form:form commandName="attendee">
         <form:errors path="*" cssClass="errorblock" element="div"/>
-        <label for="textinput1">Enter name:</label>
+        <label for="textinput1"><spring:message code="attendee.name"/> </label>
         <form:input path="name" cssErrorClass="error"/>
         <form:errors path="name" cssClass="error"/>
         <br/>
-        <label for="textinput2">Enter email Address:</label>
+        <label for="textinput2"><spring:message code="attendee.email.address"/></label>
         <form:input path="emailAddress" cssErrorClass="error"/>
         <form:errors path="emailAddress" cssClass="error"/>
         <br/>
