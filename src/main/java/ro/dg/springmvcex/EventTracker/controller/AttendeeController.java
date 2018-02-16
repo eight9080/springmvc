744f9b42774cc69c2a61583c2d1aa2b90423722d
eight9080 <eight9080@gmail.com>
2018-02-17 00:13:09 +0200

    validation

--- src/main/java/ro/dg/springmvcex/EventTracker/controller/AttendeeController.java
+++ src/main/java/ro/dg/springmvcex/EventTracker/controller/AttendeeController.java
@@ -2,10 +2,12 @@ package ro.dg.springmvcex.EventTracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ro.dg.springmvcex.EventTracker.model.Attendee;
+
+import javax.validation.Valid;
 
 @Controller
 public class AttendeeController {
@@ -20,11 +22,16 @@ public class AttendeeController {
         return "attendee";
     }
 
-
     @RequestMapping(value = "/attendee", method = RequestMethod.POST)
-    public String processAttendee(@ModelAttribute("attendee") Attendee attendee){
+    public String processAttendee(@Valid Attendee attendee, BindingResult result,
+                                  Model model){
 
         System.out.println("Attendee: " + attendee);
+
+        if(result.hasErrors()){
+            return "attendee";
+        }
+
         return "redirect:index.html";
     }
 
--- src/main/java/ro/dg/springmvcex/EventTracker/model/Attendee.java
+++ src/main/java/ro/dg/springmvcex/EventTracker/model/Attendee.java
@@ -1,8 +1,16 @@
 package ro.dg.springmvcex.EventTracker.model;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Size;
+
 public class Attendee {
 
+    @Size(min = 2, max = 30)
     private String name;
+
+    @NotEmpty @Email
     private String emailAddress;
 
     public String getName() {
